ef823b4c10796a985a7a94884cb4c19c98365419
Alexei Marusov <88613624+MarusovAlexei@users.noreply.github.com>
2022-01-09 22:15:10 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+
+module.exports = function reverse(n) {
+  if (n < 0) n = n * (-1);
+
+  var numToStr = n + '';
+  var result = numToStr[numToStr.length - 1];
+
+  for (var i = numToStr.length - 2; i >= 0; i--) {
+    result += numToStr[i];
+  }
+  return result;
 }
